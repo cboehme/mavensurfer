1f3ddf2f53507927fa8bf476457f87114bfb3e43
Christoph Böhme <christoph@b3e.net>
2017-11-05 18:14:23 +0100

    Pass GAV of artifact to PomBean

--- svnfairy-browser/src/main/java/de/dnb/tools/svnfairy/browser/PomBean.java
+++ svnfairy-browser/src/main/java/de/dnb/tools/svnfairy/browser/PomBean.java
@@ -7,8 +7,38 @@ import javax.inject.Named;
 @RequestScoped
 public class PomBean {
 
+    private String groupId;
+
+    private String artifactId;
+
+    private String version;
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
     public String getArtifactId() {
-        return "artifact-id";
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getGav() {
+        return groupId + ":" + artifactId + ":" + version;
     }
 
 }
