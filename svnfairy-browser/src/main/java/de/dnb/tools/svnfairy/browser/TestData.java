d001a18749eaa8cf3e4bb6f6ff36d7fc1c83575a
Christoph Böhme <christoph@b3e.net>
2017-11-07 07:08:21 +0100

    Fill database with test records on startup

---
+++ svnfairy-browser/src/main/java/de/dnb/tools/svnfairy/browser/TestData.java
@@ -0,0 +1,36 @@
+package de.dnb.tools.svnfairy.browser;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+import de.dnb.tools.svnfairy.ArtifactId;
+import de.dnb.tools.svnfairy.GroupId;
+import de.dnb.tools.svnfairy.MavenProject;
+import de.dnb.tools.svnfairy.Packaging;
+import de.dnb.tools.svnfairy.Version;
+import de.dnb.tools.svnfairy.browser.db.MavenProjectRepository;
+
+@ApplicationScoped
+public class TestData {
+
+    @Inject
+    private MavenProjectRepository repository;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) ServletContext init) {
+        populate();
+    }
+
+    private void populate() {
+        MavenProject project = new MavenProject("test.xml");
+        project.setGroupId(GroupId.of("org.metafacture"));
+        project.setArtifactId(ArtifactId.of("metafacture-commons"));
+        project.setVersion(Version.of("5.0.0-rc1"));
+        project.setPackaging(Packaging.of("jar"));
+        repository.create(project);
+    }
+
+}
