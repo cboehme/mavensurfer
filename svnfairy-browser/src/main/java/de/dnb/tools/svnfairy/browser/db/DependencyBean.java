d7f1b7b4f4af0ffc96cfbf99184408b38baeb216
Christoph Böhme <c.boehme@dnb.de>
2017-11-14 16:54:33 +0100

    Add JPA bean for dependencies

---
+++ svnfairy-browser/src/main/java/de/dnb/tools/svnfairy/browser/db/DependencyBean.java
@@ -0,0 +1,27 @@
+package de.dnb.tools.svnfairy.browser.db;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dependency")
+public class DependencyBean {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @ManyToOne
+    PomBean owner;
+
+    String groupId;
+    String artifactId;
+    String version;
+    String classifier;
+    String type;
+    String scope;
+    boolean optional;
+}
