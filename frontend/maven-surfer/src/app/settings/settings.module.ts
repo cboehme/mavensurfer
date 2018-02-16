8fc3b74ab650d3b32431b51b4ad2824e98d7f3a7
Christoph Böhme <christoph@b3e.net>
2018-02-16 07:10:59 +0100

    Add form for settings.xml

--- frontend/maven-surfer/src/app/settings/settings.module.ts
+++ frontend/maven-surfer/src/app/settings/settings.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
+import {ClarityModule} from "@clr/angular";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule
   ],
   declarations: [SettingsComponent]
 })
