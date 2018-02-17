91ad10d8f2403b2b894cdf77627b08a9798e4a69
Christoph Böhme <christoph@b3e.net>
2018-02-17 17:15:22 +0100

    Use child routes in settings-routing

--- frontend/maven-surfer/src/app/settings/settings-routing.module.ts
+++ frontend/maven-surfer/src/app/settings/settings-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
+import { SettingsComponent } from "./settings.component";
 import { SettingsEditorComponent } from "./settings-editor.component";
 
 const settingsRoutes: Routes = [
-  { path: 'settings', component: SettingsEditorComponent }
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    children: [
+      { path: '', component: SettingsEditorComponent }
+    ]
+  }
 ];
 
 @NgModule({
