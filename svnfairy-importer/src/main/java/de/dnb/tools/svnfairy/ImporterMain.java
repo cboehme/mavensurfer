5c648953c052fd93659682717a9a40e94afe4285
Christoph Böhme <christoph@b3e.net>
2018-10-25 07:58:56 +0200

    Remove fixed path for local Maven repository
    The location of the local maven repository must now be passed as an
    argument to the program.

--- svnfairy-importer/src/main/java/de/dnb/tools/svnfairy/ImporterMain.java
+++ svnfairy-importer/src/main/java/de/dnb/tools/svnfairy/ImporterMain.java
@@ -1,5 +1,6 @@
 package de.dnb.tools.svnfairy;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.slf4j.Logger;
@@ -12,28 +13,31 @@ import de.dnb.tools.svnfairy.repositories.pomfile.PomFileRepository;
 
 public class ImporterMain {
 
-    private static final Logger log = LoggerFactory.getLogger(
-            ImporterMain.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ImporterMain.class);
 
     private final PomFileRepository pomFileRepository;
     private final PomFileProcessor pomFileProcessor;
 
-    private ImporterMain() {
+    private ImporterMain(Path repositoryLocation) {
 
-        pomFileRepository = new LocalMavenRepository(
-                Paths.get("/home/christoph/Code/svnfairy/svnfairy-importer/testdata"));
+        pomFileRepository = new LocalMavenRepository(repositoryLocation);
         pomFileProcessor = new PomFileProcessor();
     }
 
     public static void main(String[] args) {
 
-        new ImporterMain().run();
+        if (args.length != 1) {
+            LOG.error("Program argument missing. " +
+                    "Please provide the location of the local Maven repository.");
+            return;
+        }
+        new ImporterMain(Paths.get(args[0])).run();
     }
 
     private void run() {
 
         for (PomFile pomFile : pomFileRepository.getPoms()) {
-            log.info("Send POM to processing engine: {}", pomFile);
+            LOG.info("Send POM to processing engine: {}", pomFile);
             pomFileProcessor.sendToProcessingEngine(pomFile);
         }
 
